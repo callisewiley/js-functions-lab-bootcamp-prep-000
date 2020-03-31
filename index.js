6c13e28b55bd1db00f3fe0b6a1f09d4eff6f94b0
Callise Wiley <callise.wiley@outlook.com>
2020-03-31 20:46:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidayTo(name){
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdownn(holiday, days){
+function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
 }
