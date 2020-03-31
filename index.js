04b69fb7f502a990acffc58e17ae1d043c57d300
Callise Wiley <callise.wiley@outlook.com>
2020-03-31 20:42:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function happyHolidays(){
   return "Happy Holidays!"
 }
-function happyHolidaysTo(name){
+function happyHolidayTo(name){
   console.log(`Happy holidays, ${name}!`)
 }
 function happyCustomHolidayTo(holiday, name){
